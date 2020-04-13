03103b12c30bd230708c31bd46608ef6fd15f9d3
zhenya11114 <fatality14nik@gmail.com>
2020-04-13 10:52:46 +0300

    fixed some stuff in lab1

--- lab1/src/count_summ.sh
+++ lab1/src/count_summ.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 summ=0
+num_args=0
 for var in "$@"
 do
+num_args=$(($num_args + 1))
 summ=$(($summ + $var))
 done
-echo "summ = $summ"
+avg_summ=$(($summ / $num_args))
+echo "avg_summ = $avg_summ"
